2e2cd996099138119d6a7b24334bb93bd9320960
Kasie <kchaplick92@gmail.com>
2019-02-27 22:22:01 -0500

    game logic pseduo code

---
+++ assets/javascript/game.js
@@ -0,0 +1,75 @@
+//global variables
+var magicNumber; 
+var crystalCount = 0;
+var win = 0;
+var losses = 0;
+var gemValue = 0;
+
+
+//initiate game function
+
+    //reset wins and losses
+
+    //reset gemValue
+
+    //generate Magic Number 
+
+    //set crystal count to 0
+
+//generate random magic number between 19 - 120
+    //create div
+
+    //add classes
+
+    //random function
+    
+    //append
+
+//generate gem values
+    //for loop to go through all the gems
+    
+    //add value attribute
+
+    //random number assignment between 1 - 12
+
+    //assign number to attribute
+
+//onclick event to update crystal count
+    //assign onclick event to all gems
+    
+    //update crystalcount variable 
+
+    //add div and pass it crystal count
+
+    //append 
+
+ //if statement for magic number matching crystal count
+    //if statement
+    
+        //crystal count = magic number 
+
+        //add to wins
+
+        //reset magic number
+
+        //reset gem values
+
+        //set crystal count to 0
+
+//if statement for crystal count going over magic number
+    //if statement
+        
+        //crystal count > magic number
+
+        //add to losses
+
+        //reset magic number
+
+        //reset gem values
+
+        //set crystal count to 0
+   
+        
+
+
+
