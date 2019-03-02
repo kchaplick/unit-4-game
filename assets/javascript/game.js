2043dea784a5b36725d96f9035987f5735e0a3d6
Kasie <kchaplick92@gmail.com>
2019-03-02 18:49:25 -0500

    win/loss logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,13 +11,14 @@ var usedValues = [0];
 function gameSet() {
     //reset wins and losses
     wins = 0;
+    losses = 0;
     //set crystal count to 0
     crystalCount = 0;
+    $(".crystalCounterContainer").html(crystalCount);
     //reset gemValue
-
+    assignGemVal(1, 12);
     //generate Magic Number 
-
-   
+    randomNum(19, 120);
 }
 gameSet();
 
@@ -26,73 +27,74 @@ gameSet();
 //generate random magic number between 19 - 120
 function randomNum(min, max) {
     //create div
-    var magicDiv = $("<div>")
+    var magicDiv = $("<div>");
     //add classes
-    $(magicDiv).addClass("magic-div")
+    $(magicDiv).addClass("magic-div");
     //random function
     magicNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-
-    $(magicDiv).append(magicNumber)
-    //append
+    //add number to div
+    $(magicDiv).append(magicNumber);
+    //append to container
     $(".magicContainer").append(magicDiv);
-}
-randomNum(19, 120);
+};
 
 //generate gem values for each img    
 function assignGemVal(min, max) {
-
-
     //give each img an attribute and assign gemVal
     $(".crystal").each(function () {
-        //random number assignment between 1 - 12
+        //random number assignment between 1 - 12 that checks usedVal to prevent repeats
         while (jQuery.inArray(gemValues, usedValues) != -1) {
             gemValues = Math.floor(Math.random() * (max - min + 1)) + min;
-        }
+        };
         //find crystal imgs add value attribute
         $(this).attr("data-val", gemValues);
+        //push gemVal to usedVal array
         usedValues.push(gemValues);
     })
-}
-assignGemVal(1, 12);
+};
+
 
 //onclick event to update crystal count
-//assign onclick event to all gems
-$(".crystal").click(function () {
+    //assign onclick event to all gems
+    $(".crystal").click(function () {
     //update crystalcount variable with value of gem
-    var getGemVal = parseInt($(this).attr("data-val"))
-    crystalCount = crystalCount + getGemVal
-    console.log("Value of data attr " + getGemVal);
-    console.log("New value of crystalCount " + crystalCount);
+    var getGemVal = parseInt($(this).attr("data-val"));
+    crystalCount = crystalCount + getGemVal;
     //update html in container
     $(".crystalCounterContainer").html(crystalCount);
-});
 
-
- //if statement for magic number matching crystal count
-    //if statement
-
-        //crystal count = magic number 
-
+     //if statement for magic number matching crystal count
+    if( crystalCount == magicNumber){
+        //set crystal count to 0
+        crystalCount = 0;
+        $(".crystalCounterContainer").html(crystalCount);
         //add to wins
-
+        wins++
+        $(".win-count").html(wins);
         //reset magic number
-
+        $(".magic-div").remove();
+        randomNum(19, 120);
         //reset gem values
-
+        usedValues = [];
+        assignGemVal(1, 12);
+    }
+    //if statement for magic number going over crystal count
+    if (crystalCount > magicNumber){
         //set crystal count to 0
-
-//if statement for crystal count going over magic number
-    //if statement
-
-        //crystal count > magic number
-
+        crystalCount = 0;
+        $(".crystalCounterContainer").html(crystalCount);
         //add to losses
-
+        losses++
+        $(".loss-count").html(losses);
         //reset magic number
-
+        $(".magic-div").remove();
+        randomNum(19, 120);
         //reset gem values
-
-        //set crystal count to 0
+        usedValues = [];
+        assignGemVal(1, 12);
+    }       
+//end on click event    
+});
 
 
 
